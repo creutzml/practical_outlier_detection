2a781d9bb0301a01340a3068f786e333ada2f7bc
Michael Creutzinger <creutzml@Jordans-MacBook-Pro-2.local>
2024-01-13 14:45:26 -0700

    edited plotting script to only include code relevant to figures in the manuscript

--- R Code/plotting_script.R
+++ R Code/plotting_script.R
@@ -47,55 +47,5 @@ for (i in 1:9) {
 
 do.call("grid.arrange", c(model_list, ncol = 3))
 
-## Berkeley growth study
-# data
-data("growth")
-growth_data <- growth$hgtf[,1:10] %>%
-  data.frame() %>%
-  mutate(age = growth$age) %>%
-  pivot_longer(cols = -age, names_to = "girl", values_to = "height")
 
-ggplot() + 
-  geom_line(data = growth_data, 
-            aes(x = age, y = height, color = girl)) +
-  geom_point(data = growth_data, 
-             aes(x = age, y = height, color = girl)) +
-  geom_vline(xintercept = seq(3.12, 18, length.out = 8), linetype = "dashed") +
-  guides(color = "none") +
-  labs(x = "Age (years)", 
-       y = "Height (cm)", 
-       caption = paste0("Heights collected on 10 females at 31 ages",
-                        ", from the Berkeley Growth Study")) +
-  theme_bw() +
-  theme(text=element_text(size=24))
-
-## Gait cycle data
-data("gait")
-hip_angle <- gait[,,1] %>%
-  data.frame() %>%
-  mutate(time = as.numeric(rownames(gait[,,1]))) %>%
-  pivot_longer(-time, names_to = "boy", values_to = "angle") %>%
-  mutate(location = "Hip")
-knee_angle <- gait[,,2] %>%
-  data.frame() %>%
-  mutate(time = as.numeric(rownames(gait[,,2]))) %>%
-  pivot_longer(-time, names_to = "boy", values_to = "angle") %>%
-  mutate(location = "Knee")
-
-gait_data <- rbind.data.frame(hip_angle, knee_angle) %>%
-  as.data.frame()
-
-ggplot() +
-  geom_line(data = gait_data, 
-            aes(x = time, y = angle, color = boy)) +
-  geom_point(data = gait_data, 
-             aes(x = time, y = angle, color = boy)) +
-  facet_grid(rows = vars(location)) +
-  guides(color = "none") +
-  labs(x = "Time (proportion of gait cycle)", 
-       y = "Angle (degrees)",
-       title = "Multivariate Random Sample of Gait Cycle Data",
-       subtitle = "n = 39, p = 2, T = 20") +
-  theme_bw() +
-  theme(text=element_text(size=24))
   
